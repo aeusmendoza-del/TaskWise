b825c3732630b38c814424a18e48c156cc2b7ce8
calvinkleindejesus-ship-it <calvinkleindejesus@gmail.com>
2026-09-11 14:07:04 +0800

    added a new code

---
+++ app/src/test/java/quarter2/Dejesus_MiniPeta1.java
@@ -0,0 +1,21 @@
+
+. quarter2;
+
+import org.junit.Test;
+
+public class quarter2 {
+    public static class De jesus {
+        @Test
+        public void printMyProfile() {
+            String myName = "calvin";
+            String petNAme = "Slater";
+            String favFood = "Takoyaki";
+            int myAge = 16;
+
+            System.out.println("--- MY DIGITAL PROFILE");
+            System.out.println("Hi my name is calvin " + myName + " And I am " + myAge + " years old. ");
+            System.out.println(" I have a pet dog named " + petNAme + ".");
+            System.out.println(" If i could eat something " + favFood + " And i eat it everytime! ");
+        }
+    }
+}
