617383d79f7c86ab214def5e335e260deb711188
Bonacruz <markcynth800@gmail.com>
2026-09-12 14:04:31 +0800

    new code

---
+++ app/src/test/java/quarter2/Bonacruz_MiniPeta1.java
@@ -0,0 +1,18 @@
+package quarter2;
+
+import org.junit.Test;
+
+public class Bonacruz_MiniPeta1 {
+    @Test
+    public void printMyProfile() {
+        String myName = "Bona";
+        String petNAme = "Slater";
+        String favFood = "Dog";
+        int myAge = 16;
+
+        System.out.println("--- MY DIGITAL PROFILE");
+        System.out.println("Hi my name is bona " + myName + " And I am " + myAge + " years old. ");
+        System.out.println(" I have a pet dog named " + petNAme + ".");
+        System.out.println(" If i could eat something " + favFood + " And i eat it everytime! ");
+    }
+}
