4ff61b8f671d91cfdeadcf8caf17696aba81435a
HERNANDEZ <khiel.harrish06@gmail.com>
2026-09-14 13:14:47 +0800

    added new code

---
+++ app/src/test/java/quarter2/Hernandez_MiniPeta1.java
@@ -0,0 +1,18 @@
+package quarter2;
+
+import org.junit.Test;
+
+public class Hernandez_MiniPeta1 {
+    @Test
+    public void printMyProfile() {
+        String myName = "Khiel";
+        String petNAme = "Princess";
+        String favFood = "karekare";
+        int myAge = 16;
+
+        System.out.println("--- MY DIGITAL PROFILE");
+        System.out.println("Hi my name is Khiel " + myName + " And I am " + myAge + " years old. ");
+        System.out.println(" I have a pet dog named " + petNAme + ".");
+        System.out.println(" If i could eat something " + favFood + " And i eat it everytime! ");
+    }
+}
