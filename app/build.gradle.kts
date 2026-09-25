c18159f6c1aad6c1676f538059eb9ed03f92ac24
HERNANDEZ <khiel.harrish06@gmail.com>
2026-09-25 14:06:46 +0800

    added new code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,11 +18,6 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        externalNativeBuild {
-            cmake {
-                cppFlags += ""
-            }
-        }
     }
 
     buildTypes {
@@ -35,12 +30,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
-    }
-    externalNativeBuild {
-        cmake {
-            path = file("src/main/cpp/CMakeLists.txt")
-            version = "3.22.1"
-        }
     }
 }
 
--- app/src/test/java/quarter2/Bonacruz_MiniPeta1.java
+++ app/src/test/java/quarter2/Bonacruz_MiniPeta1.java
@@ -12,7 +12,7 @@ public class Bonacruz_MiniPeta1 {
 
         System.out.println("--- MY DIGITAL PROFILE");
         System.out.println("Hi my name is bona " + myName + " And I am " + myAge + " years old. ");
-        System.out.println(" I have a pet dog named " + petNAme + " . ");
+        System.out.println(" I have a pet dog named " + petNAme + ".");
         System.out.println(" If i could eat something " + favFood + " And i eat it everytime! ");
     }
 }
